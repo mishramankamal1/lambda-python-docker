3a3d8e673c8f63884e778f2fab106c4610d71a75
Mankamal Mishra <mishramankamal1@gmail.com>
2024-04-13 16:18:21 +0530

    edited return statement

--- app.py
+++ app.py
@@ -23,8 +23,7 @@ def lambda_handler(event, context):
         
     # TODO implement
     return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'statusCode': 200
     }
 
 
